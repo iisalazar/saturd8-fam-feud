281e982a3032a01f0601891c7c4df0dadf2a86a3
Ian Salazar <iisalazar@up.edu.ph>
2021-04-09 20:12:09 +0800

    Added active class names and design fro Navbar items

--- src/components/molecules/Navbar/Navbar.tsx
+++ src/components/molecules/Navbar/Navbar.tsx
@@ -1,32 +1,32 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "./Navbar.scss";
 
 interface Props {
-	logoText: string;
+  logoText: string;
 }
 
 function Navbar(props: Props) {
-	return (
-		<nav className="navbar">
-			<div className="left">
-				<span className="logo-text">{props.logoText || "Logo"}</span>
-			</div>
-			<div className="right">
-				<Link to="/" className="item">
-					Dashboard
-				</Link>
-				<Link to="/answers" className="item">
-					Answers
-				</Link>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar">
+      <div className="left">
+        <span className="logo-text">{props.logoText || "Logo"}</span>
+      </div>
+      <div className="right">
+        <NavLink exact to="/" className="item" activeClassName="active">
+          Dashboard
+        </NavLink>
+        <NavLink exact to="/answers" className="item" activeClassName="active">
+          Answers
+        </NavLink>
+      </div>
+    </nav>
+  );
 }
 
 Navbar.propTypes = {
-	logoText: PropTypes.string,
+  logoText: PropTypes.string,
 };
 
 export default Navbar;
